26af1248b0af3cd65645a12a2537813a88a121ae
Ffej Caplan <ffej.caplan@gmail.com>
2020-06-10 14:35:39 -0400

    refactored inqurirer promps

--- app.js
+++ app.js
@@ -2,61 +2,84 @@ const inquirer = require("inquirer");
 const express = require("express");
 const app = express();
 const path = require("path");
+const merge = require("lodash.merge");
 const fs = require("fs");
+const htmlRender = require("./lib/htmlRender");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const basicQuestions = [
-  { type: "input", name: "name", message: "Name" },
+app.listen(PORT, () => {});
+
+const selectRole = [
   {
     type: "list",
     name: "role",
     message: "Select Your Job Type",
-    choices: ["Manager", "Engineer", "Intern"],
+    choices: [
+      "Engineer",
+      "Intern",
+      "I don't want to add any more team members",
+    ],
   },
 ];
 
+const addEmployees = () => {
+  inquirer.prompt(selectRole).then((response) => {
+    if (response.role == "Engineer") {
+      inquirer.prompt(engineerQuestions).then((answers2) => {
+        answersArray.push(answers2);
+        console.log(answersArray);
+        addEmployees();
+      });
+    } else if (response.role == "Intern") {
+      inquirer.prompt(internQuestions).then((answers2) => {
+        answersArray.push(answers2);
+        console.log(answersArray);
+        addEmployees();
+      });
+    } else {
+      htmlRender.render(answersArray);
+    }
+  });
+};
+
 //object Engineer questions
 const engineerQuestions = [
-  { type: "input", name: "q1", message: "ID:" },
-  { type: "input", name: "q2", message: "Email" },
-  { type: "input", name: "q3", message: "GitHub Account" },
+  { type: "input", name: "name", message: "Name" },
+  { type: "input", name: "id", message: "ID:" },
+  { type: "input", name: "email", message: "Email" },
+  { type: "input", name: "github", message: "GitHub Account" },
 ];
 //object Manager questions
 const managerQuestions = [
-  { type: "input", name: "q1", message: "ID:" },
-  { type: "input", name: "q2", message: "Email" },
-  { type: "number", name: "q3", message: "Office Number" },
+  { type: "input", name: "name", message: "Name" },
+  { type: "input", name: "id", message: "ID:" },
+  { type: "input", name: "email", message: "Email" },
+  { type: "number", name: "officeNumber", message: "Office Number" },
 ];
 
 //object intern questions
 const internQuestions = [
-  { type: "input", name: "q1", message: "ID:" },
-  { type: "input", name: "q2", message: "Email" },
-  { type: "input", name: "q3", message: "School" },
+  { type: "input", name: "name", message: "Name" },
+  { type: "input", name: "id", message: "ID:" },
+  { type: "input", name: "email", message: "Email" },
+  { type: "input", name: "school", message: "School" },
 ];
-
+const answersArray = [];
 //inquirer select role
-inquirer.prompt(basicQuestions).then((answers) => {
-  if (answers.role == "Manager") {
-    inquirer.prompt(managerQuestions).then((answers2) => {
-      const answerObj = { ...answers, ...answers2 };
-      console.log(answerObj);
-    });
-  } else if (answers.role == "Engineer") {
-    inquirer.prompt(engineerQuestions).then((answers2) => {
-      const answerObj = { ...answers, ...answers2 };
-      console.log(answerObj);
-    });
-  } else if (answers.role == "Intern") {
-    inquirer.prompt(internQuestions).then((answers2) => {
-      const answerObj = { ...answers, ...answers2 };
-      console.log(answerObj);
-    });
-  }
+inquirer.prompt(managerQuestions).then((answers) => {
+  answersArray.push(answers);
+  addEmployees();
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./templates/main.html"));
 });
 
 //if x role is selected
